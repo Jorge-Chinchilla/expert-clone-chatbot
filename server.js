64cd51385ad25f81a1048d94e749605c925c87e7
Jorge Chinchilla <jorgech.innovates@gmail.com>
2025-04-19 21:01:02 -0400

    Tested changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"]=0;
 const express = require('express');
-const path = express('path');
+const path = require('path');
 const { OpenAIAPI } = require('./openai');
 
 const app = express();
@@ -10,17 +10,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.post('/getChatbotResponse', async (req, res) => {
     const userMessage = req.body.userMessage;
 
+    // Use OpenAI API to generate a response
     const chatbotResponse = await OpenAIAPI.generateResponse(userMessage);
 
+    // Send the response back to the client
     res.json({ chatbotResponse });
 });
-
 app.listen(port, () => {
-    console.log(`Listening on ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
